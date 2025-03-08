be29a2fa52da94944dbd98eca9f3c1570771e77e
AdrianGC05 <coronita2212adrian@gmail.com>
2025-03-07 23:28:11 -0600

    Actividad 7

--- Actividad 7/Actividad7_unit_test.py
+++ Actividad 7/Actividad7_unit_test.py
@@ -1,12 +1,18 @@
-#unit testing
+#Unit Testing
 import unittest
 import Actividad7_1_numeros
 
-class TestRacionales(unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
 
+    def test_upper(self):
+        #crear classe
+        self.assertEqual('foo'.upper(), 'FOO')
+
+class TestRacionales(unittest.TestCase):
     def test_print_hello(self):
-        #crear clase
-        self.assertEqual('Hello Adrian')
+        m = 0
+        racionales = Actividad7_1_numeros.Racionales(m)
+        self.assertEqual(racionales.print_hello(), "Hello Adrian")
 
 if __name__ == '__main__':
     unittest.main()
